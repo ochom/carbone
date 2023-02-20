23d3b404c047b8769d3a4eb544a880c8af9498a3
ochom <ochomrichard752@gmail.com>
2023-02-20 17:40:30 +0300

    implement: cache

--- index.js
+++ index.js
@@ -6,6 +6,76 @@ const { getTemplate, getData } = require("./helpers");
 const libre = require("libreoffice-convert");
 libre.convertAsync = require("util").promisify(libre.convert);
 
+let cache = {
+  data: [],
+  buffer: null,
+  createdAt: null,
+};
+
+const streamData = async () => {
+  setInterval(async () => {
+    const data = await getData();
+
+    // check if data has changed
+    if (JSON.stringify(data) !== JSON.stringify(cache.data)) {
+      console.log("Data changed");
+
+      // update cache
+      cache.data = data;
+      cache.createdAt = null;
+    }
+  }, 1000 * 60 * 5);
+};
+
+const loadCache = async () => {
+  if (cache.createdAt) {
+    const now = new Date();
+    const diff = now - cache.createdAt;
+    const minutes = Math.floor(diff / 1000 / 60);
+
+    if (minutes < 5) {
+      console.log("Using cache");
+      return new Promise((resolve) => {
+        resolve(cache);
+      });
+    }
+
+    console.log("Cache expired");
+  }
+
+  const template = getTemplate();
+  if (cache.data.length === 0) {
+    cache.data = await getData();
+  }
+
+  return new Promise((resolve, reject) => {
+    // template name with timestamp
+    const input = `/tmp/template-${Date.now()}.docx`;
+
+    // write to disk
+    fs.writeFileSync(input, Buffer.from(template, "base64"));
+
+    carbone.render(input, cache.data, async (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      // Convert it to pdf format with undefined filter (see Libreoffice docs about filter)
+      const pdfBuf = await libre.convertAsync(result, ".pdf", undefined);
+
+      cache.buffer = pdfBuf;
+
+      cache.createdAt = new Date();
+
+      return resolve(cache);
+    });
+  });
+};
+
+// start streaming data
+console.log("Starting data stream");
+streamData();
+
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 
@@ -67,30 +137,9 @@ app.post("/generate", async (req, res) => {
 });
 
 app.get("/download-games", async (req, res) => {
-  const data = await getData();
-  const template = getTemplate();
-
-  // template name with timestamp
-  const input = `/tmp/template-${Date.now()}.docx`;
-
-  // convert base64 to binary
-  const binary = Buffer.from(template, "base64");
-
-  // write to disk
-  fs.writeFileSync(input, binary);
-
-  carbone.render(input, data, async (err, result) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    }
-
-    // Convert it to pdf format with undefined filter (see Libreoffice docs about filter)
-    const pdfBuf = await libre.convertAsync(result, ".pdf", undefined);
-
-    // send to client
-    res.contentType("application/pdf");
-    res.send(pdfBuf);
-  });
+  const cache = await loadCache();
+  res.contentType("application/pdf");
+  res.send(cache.buffer);
 });
 
 const PORT = process.env.PORT || 3000;
