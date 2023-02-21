05bf640b387723e992a958978e8b88112e9ee575
ochom <ochomrichard752@gmail.com>
2023-02-21 19:21:53 +0300

    feat: parse decimal

--- helpers.js
+++ helpers.js
@@ -17,6 +17,16 @@ const getData = async () => {
     const newData = res.data.map((d) => {
       const startTime = moment(d.startTime).format("DD/MM HH:mm");
 
+      // format the odds to 2 decimal places
+      d.homeOdds = parseFloat(d.homeOdds).toFixed(2);
+      d.awayOdds = parseFloat(d.awayOdds).toFixed(2);
+      d.drawOdds = parseFloat(d.drawOdds).toFixed(2);
+
+      d.meta.un = parseFloat(d.meta.un || 0).toFixed(2);
+      d.meta.ov = parseFloat(d.meta.ov || 0).toFixed(2);
+      d.meta.gg = parseFloat(d.meta.gg || 0).toFixed(2);
+      d.meta.ng = parseFloat(d.meta.ng || 0).toFixed(2);
+
       return { ...d, startTime };
     });
     return newData;
